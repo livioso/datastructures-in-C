a6a6f1f00be4aff271f380fe3a8049fa4171dadf
Livio Bieri <livio@livio.li>
2013-12-25 20:53:17 +0100

    First empty test added

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 default: *.c *.h
 	mkdir -p bin
-	gcc -std=c99 -o bin/test_trie test_trie.c trie.c
+	gcc -std=c99 -o bin/test_trie test_trie.c
 	./bin/test_trie
---
+++ test_trie.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#include "stdbool.h"
+#include "assert.h"
+
+void testCreateTrie ()
+{
+	printf(" >> testCreateTrie: ");
+	assert(false == true);
+}
+
+
+int main()
+{
+	printf("Running tests now... \n");
+
+	testCreateTrie();
+
+	return 0;
+}
