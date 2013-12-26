74231f245ca275947bf58131dc2d895be465e4f0
Livio Bieri <livio@livio.li>
2013-12-26 12:47:32 +0100

    Minor refactoring

--- test_trie.c
+++ test_trie.c
@@ -5,21 +5,27 @@
 
 void testCreateTrie ()
 {
-	printf(" >> testCreateTrie: ");
+	printf(" >> testCreateTrie \n");
 
 	trieNode* underTest = createNode('a', 1);
-
 	assert(underTest != NULL);
+}
+
+void testKeyValue ()
+{
+	printf(" >> testKeyValue \n");
+
+	trieNode* underTest = createNode('a', 1);
 	assert('a' == underTest->key);
 	assert(1 == underTest->value);
 }
-
 
 int main()
 {
 	printf("Running tests now... \n");
 
 	testCreateTrie();
+	testKeyValue();
 
 	return 0;
 }
